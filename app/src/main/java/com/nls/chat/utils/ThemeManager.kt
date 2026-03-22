2a3797142e77a2020f86488f589a95ae372e8783
nlsetenament-dotcom <nls.etenament@gmail.com>
2026-03-22 09:14:00 -0500

    Actualizar ThemeManager.kt

--- app/src/main/java/com/nls/chat/utils/ThemeManager.kt
+++ app/src/main/java/com/nls/chat/utils/ThemeManager.kt
@@ -3,16 +3,22 @@ package com.nls.chat.utils
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import com.nls.chat.data.local.PreferencesManager
+import com.nls.chat.utils.Constants
 
 object ThemeManager {
 
     fun applyTheme(context: Context) {
         val theme = PreferencesManager(context).appTheme
+
         when (theme) {
             Constants.THEME_DARK,
             Constants.THEME_BW,
-            Constants.THEME_AMOLED -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            else                   -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            Constants.THEME_AMOLED -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
         }
     }
 
